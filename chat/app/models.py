dc31488c6238a9b08e5957f3af6240adbafd939d
anacarneirosv <ana.carneiro@convatec.com>
2022-09-07 12:42:12 +0100

    Added models

--- chat/app/models.py
+++ chat/app/models.py
@@ -1,3 +1,15 @@
+from tkinter import CASCADE
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    avatar = models.ImageField()
+    comment = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    parent_comment = models.ForeignKey('self', on_delete=CASCADE)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
