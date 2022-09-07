e80b49a643a8f71ff7cec3e48a7dae332986489b
anacarneirosv <ana.carneiro@convatec.com>
2022-09-07 13:15:18 +0100

    Fix models and added migrations

--- chat/app/models.py
+++ chat/app/models.py
@@ -1,14 +1,14 @@
-from tkinter import CASCADE
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 
 
 class Post(models.Model):
-    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     avatar = models.ImageField()
-    comment = models.TextField()
+    comment = models.CharField(max_length=280)
     created_date = models.DateTimeField(default=timezone.now)
-    parent_comment = models.ForeignKey('self', on_delete=CASCADE)
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE)
 
     def publish(self):
         self.published_date = timezone.now()
