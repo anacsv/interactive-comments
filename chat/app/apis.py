78e31be601d32841fed4a7df9863a820d47c42db
anacarneirosv <ana.carneiro@convatec.com>
2022-09-13 20:15:30 +0100

    Auth upgrade

--- chat/app/apis.py
+++ chat/app/apis.py
@@ -1,10 +1,12 @@
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework import viewsets, permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.filter(parent_post__isnull=True).order_by('created_date')
     serializer_class = PostSerializer
+    authentication_classes = [JWTAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def delete(self, request, pk, format=None):
--- chat/chat/urls.py
+++ chat/chat/urls.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView, TokenVerifyView
 from app import apis
 
 router = routers.DefaultRouter()
@@ -7,4 +8,7 @@ router.register(r'posts', apis.PostViewSet, 'posts')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))]
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    ]
