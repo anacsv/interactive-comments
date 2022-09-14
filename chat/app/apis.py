c1344a2d5b559d1402e253bd2c32420a9cb0799c
anacarneirosv <ana.carneiro@convatec.com>
2022-09-14 17:50:45 +0100

    Changed de filter to list all

--- chat/app/apis.py
+++ chat/app/apis.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, permissions
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.filter(parent_post__isnull=True).order_by('created_date')
+    queryset = Post.objects.order_by('created_date')
     serializer_class = PostSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [permissions.IsAuthenticated]
