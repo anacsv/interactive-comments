24a72a24b3de2b845259b7d15c774347c4faf26b
anacarneirosv <ana.carneiro@convatec.com>
2022-09-20 15:29:49 +0100

    Added control acess

--- chat/app/apis.py
+++ chat/app/apis.py
@@ -1,33 +1,19 @@
-from .models import Post, CustomUser
-from app.serializers import PostSerializer, UserSerializer
-from rest_framework import viewsets, permissions
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
-from app.permissions import IsLoggedInUserOrAdmin, IsAdminUser
+from .permissions import IsAdmin
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.order_by('created_date')
     serializer_class = PostSerializer
     authentication_classes = [JWTAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAdmin]
 
     def delete(self, request, pk, format=None):
         post = self.get_object(pk)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-
-
-    def get_permissions(self):
-        permission_classes = []
-        if self.action == 'create':
-            permission_classes = [AllowAny]
-        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
-            permission_classes = [IsLoggedInUserOrAdmin]
-        elif self.action == 'list' or self.action == 'destroy':
-            permission_classes = [IsAdminUser]
-        return [permission() for permission in permission_classes]
