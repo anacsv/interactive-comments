87ab018b4c5a1e27f21bae05587bd08c79118821
anacarneirosv <ana.carneiro@convatec.com>
2022-10-03 13:01:56 +0100

    Added IsSameUser permission

--- chat/app/apis.py
+++ chat/app/apis.py
@@ -4,14 +4,14 @@ from app.serializers import PostSerializer, UserSerializer
 from rest_framework import viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import viewsets
-from app.permissions import IsAdmin
+from app.permissions import IsAdmin, IsSameUser
 from rest_framework.permissions import IsAuthenticated
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.order_by('created_date')
     serializer_class = PostSerializer
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsSameUser]
 
     def delete(self, request, pk, format=None):
         post = self.get_object(pk)
--- chat/app/permissions.py
+++ chat/app/permissions.py
@@ -8,3 +8,13 @@ class IsAdmin(BasePermission):
             return True
         else:
             return request.method not in ('DELETE',)
+
+class IsSameUser(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = view.request.user
+        model_user = obj.username
+
+        if request.method in ('PUT', 'DELETE',) and (user.is_superuser or user.id == model_user.id):
+            return super().has_object_permission(request, view, obj)
+        else:
+            return False
