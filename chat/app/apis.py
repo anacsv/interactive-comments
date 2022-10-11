6d777ba0446da77308bdceac0d598d7614e81476
anacarneirosv <ana.carneiro@convatec.com>
2022-10-11 09:07:16 +0100

    Fix apis

--- chat/app/apis.py
+++ chat/app/apis.py
@@ -3,9 +3,7 @@ from django.contrib.auth.models import User
 from app.serializers import PostSerializer, UserSerializer
 from rest_framework import viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework import viewsets
 from app.permissions import IsAdmin, IsSameUser
-from rest_framework.permissions import IsAuthenticated
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.order_by('created_date')
@@ -13,10 +11,6 @@ class PostViewSet(viewsets.ModelViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsSameUser]
 
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
